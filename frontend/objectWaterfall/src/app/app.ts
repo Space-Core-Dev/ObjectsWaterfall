f7286025f1956efea1d5c27cf8d021d0823874c2
vkorenkov <vkorenkov@ussc.ru>
2025-08-02 20:37:50 +0300

    New worker settings component was added. Some fixes

--- frontend/objectWaterfall/src/app/app.ts
+++ frontend/objectWaterfall/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WorkerSettings } from "./worker-settings/worker-settings";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WorkerSettings],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"objectswaterfall.com/application/handlers"
@@ -17,6 +20,15 @@ func main() {
 		panic(err)
 	}
 	engine := gin.Default()
+
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	engine.POST("/add", handlers.Add)
 	engine.POST("/start", handlers.Start)
